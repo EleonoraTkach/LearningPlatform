5b72bfbb5d3c0ef1739b7b36e513bb4daaffc8e2
Lapidusa <dianiko2004@gmail.com>
2025-11-09 03:46:01 +0300

    initial commit

---
+++ shared/learning_platform_common/utils.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+
+class ResponseUtils:
+  @staticmethod
+  def success(message: str | None = None, **kwargs: Any) -> dict[str, Any]:
+    response: dict[str, Any] = {"result": True}
+    if message:
+      response["message"] = message
+    if kwargs:
+      response.update(kwargs)
+    return response
+
+  @staticmethod
+  def error(message: str = "Произошла ошибка") -> dict[str, Any]:
+    return {"result": False, "message": message}
